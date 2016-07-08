be7508492e7dab53a4cf5fccf92d3014f0d0c9dd
tworitdash <tworitdash@gmail.com>
2016-07-08 22:48:17 +0000

    Pi Code

--- read.py
+++ read.py
@@ -4,19 +4,20 @@ import websockets
 import json
 import time
 from random import randint
+import serial
 
-
-#ser =serial.Serial("/dev/tty.usbserial", 9600, timeout=1)
+ser =serial.Serial("/dev/ttyACM0", 9600, timeout=1)
 
 
 async def hello():
-	async with websockets.connect('ws://127.0.0.1:4000/socket/websocket') as websocket:
-		data = dict(topic="users:YWxleEBnbWFpbC5jb21hbGV4Y29sZXM=", event="phx_join", payload={}, ref=1)
-		#data = dict(topic="users:anVhbkBnbWFpbC5jb21qdWxpYTEyMw==", event="phx_join", payload={}, ref=1)
+	async with websockets.connect('ws://192.168.1.51:4000/socket/websocket') as websocket:
+		#data = dict(topic="users:YWxleEBnbWFpbC5jb21hbGV4Y29sZXM=", event="phx_join", payload={}, ref=1)
+		data = dict(topic="users:anVhbkBnbWFpbC5jb21qdWxpYTEyMw==", event="phx_join", payload={}, ref=1)
 		await websocket.send(json.dumps(data))
 		# print("joined")
 		#greeting = await websocket.recv()
 		print("Joined")
+		a = ""
 		while True:
 			msg = await retrieve()
 			await websocket.send(json.dumps(msg))
@@ -28,28 +29,33 @@ async def hello():
 			#print(control['event'])
 			if(control['event'] == "control"):
 				print(control['payload']['val'])
+				if(control['payload']['val'] == "on"):
+					a = "o"
+				if(control['payload']['val'] == "off"):
+					a = "f"
+				ser.write(a.encode())
 			
 			print("< {}".format(call))
-			time.sleep(3)
+			#time.sleep(0.1)
+
 
 async def retrieve():
+	ser.write("&".encode())
+	data = ser.readline().decode()
+	print(data)
+	load = data[7:10]
+	pf = data[25:27]
+	reading = data[34:37]
+	thd = "23"
+	output = {"load": load, "pf": pf, "reading": reading,"thd": thd}
+		
+	msg = dict(topic="users:anVhbkBnbWFpbC5jb21qdWxpYTEyMw==", event="sensor_output", payload=output, ref=None)
+ 	#data_to_be_sent = await concurrent(data)
+	print(msg)
+	return(msg)
+
+
 	
-	msg = dict(topic="users:YWxleEBnbWFpbC5jb21hbGV4Y29sZXM=", event="sensor_output", payload={"load":"1000", "pf":str(randint(0,100)), "thd":"120", "reading":"1800"}, ref=None)
-	time.sleep(3)
-	return msg
-
-		# while True:
-		# 	msg = dict(topic="users:anVhbkBnbWFpbC5jb21qdWxpYTEyMw==", event="shout", payload="hello, sir !", ref=None)
-		# 	await websocket.send(json.dumps(msg))
-		# 	#print("> {}".format(data))
-		# 	greeting = await websocket.recv()
-		# 	print("< {}".format(greeting))
-
-
-# async def retrieve():
-# 	data = ser.readline().decode()
-# 	data_to_be_sent = await concurrent(data)
-# 	return(data_to_be_sent)
 
 # async def concurrent(data):	
 # 	#while True:
